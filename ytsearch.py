6116c551aabce6ed380983d9949b1b85331045c2
luudvig <ludvigolsson@outlook.com>
2020-11-05 21:34:49 +0100

    modified fedora.yml, ytsearch.py

--- ytsearch.py
+++ ytsearch.py
@@ -1,60 +1,75 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
-from datetime import timedelta
-from json import dump, loads
-from os import environ, remove
+from json import loads
+from os import environ
 from re import match
+from requests import get
 from subprocess import CalledProcessError, PIPE, Popen, run
-from tempfile import NamedTemporaryFile
+from sys import exit
 
 parser = ArgumentParser()
-parser.add_argument('-d', '--download', action='store_true', help='download all videos')
-parser.add_argument('-n', '--number', type=int, default=1, help='number of search results')
-parser.add_argument('-r', '--resolution', type=int, default=1080, help='maximum resolution (height)')
+parser.add_argument('-d', '--download', action='store_true', help='download all selected videos')
+parser.add_argument('-n', '--number', type=int, default=5, help='number of search results (default: 5)')
+parser.add_argument('-r', '--resolution', type=int, default=720, help='maximum resolution (height) to download (default: 720)')
 parser.add_argument('search', nargs='+', help='string to search for')
+required_arguments = parser.add_argument_group('required arguments')
+required_arguments.add_argument('-k', '--api-key', required=True, help='key to use in api requests')
 arguments = parser.parse_args()
 
-bin_ytdl = run(['which', 'youtube-dl'], stdout=PIPE, stderr=PIPE, check=True, text=True).stdout.rstrip()
-bin_vlc = run(['which', 'vlc'], stdout=PIPE, stderr=PIPE, check=True, text=True).stdout.rstrip()
+bin_ytdl = run(['which', 'youtube-dl'], stdout=PIPE, check=True, text=True).stdout.rstrip()
+bin_vlc = run(['which', 'vlc'], stdout=PIPE, check=True, text=True).stdout.rstrip()
 
-command = [bin_ytdl, '--dump-single-json', '--format', 'bestvideo[height<={0}]+bestaudio/best[height<={0}]'.format(arguments.resolution)]
+def run_success(args, stdout=None, text=None):
+    while True:
+        try:
+            process = run(args, stdout=stdout, check=True, text=text)
+        except CalledProcessError:
+            continue
+        else:
+            return process
 
 if len(arguments.search) == 1 and match('https:\/\/(www\.)?youtu\.?be(\.com)?\/(watch\?v=)?[a-zA-Z0-9-_]{11}', arguments.search[0]):
-    command.append(arguments.search[0])
+    webpage_urls = [arguments.search[0]]
 else:
-    command.append('ytsearch{0}:{1}'.format(arguments.number, ' '.join(arguments.search)))
+    locator = 'https://youtube.googleapis.com/youtube/v3'
+    headers = dict(accept='application/json')
 
-while True:
+    search_payload = dict(part='id', maxResults=arguments.number, q=' '.join(arguments.search), type='video', key=arguments.api_key)
+    search_response = get('{0}/search'.format(locator), headers=headers, params=search_payload)
+
+    videos_payload = dict(part=['contentDetails', 'snippet'], id=[i['id']['videoId'] for i in search_response.json()['items']], key=arguments.api_key)
+    videos_response = get('{0}/videos'.format(locator), headers=headers, params=videos_payload)
+    videos_items = videos_response.json()['items']
+
+    for c, i in enumerate(videos_items, 1):
+        print('{0}. [{1} {2}] {3} ({4})'.format(c, i['snippet']['publishedAt'][:10], i['snippet']['channelTitle'],
+            i['snippet']['title'], i['contentDetails']['duration'][2:].lower()))
+
     try:
-        process = run(command, stdout=PIPE, stderr=PIPE, check=True, text=True)
-    except CalledProcessError:
-        continue
-    else:
-        break
+        selections = input('[ytsearch] Select video(s) to stream/download (space separated) [1]: ') or '1'
+    except KeyboardInterrupt:
+        exit('')
 
-results = loads(process.stdout)
-
-if 'entries' in results:
-    for i, e in enumerate(results['entries'], 1):
-        print('{0}. [{1}] {2} ({3})'.format(i, e['uploader'], e['title'], timedelta(seconds=e['duration'])))
-    selections = input('INPUT: Select video(s) to stream/download (space separated) [1]: ') or '1'
-    entries = list(map(lambda s : results['entries'][int(s) - 1], selections.split()))
-else:
-    entries = [results]
+    webpage_urls = ['https://www.youtube.com/watch?v={0}'.format(videos_items[int(s) - 1]['id']) for s in selections.split()]
+    print('[ytsearch] Video(s) selected: {0}'.format(' '.join(webpage_urls)))
 
 if not arguments.download:
-    e = entries.pop(0)
-    format_ids = e['format_id'].split('+')
-    formats = list(filter(lambda f : f['format_id'] in format_ids, e['formats']))
-    command = [bin_vlc, '--meta-title', e['title'], '--meta-artist', e['uploader'], formats[0]['url']]
-    if len(formats) > 1:
-        command.extend(['--input-slave', formats[1]['url']])
-    Popen(command, stdout=PIPE, stderr=PIPE)
+    ytdl_command = [bin_ytdl, '--dump-json', '--format', 'bestvideo[height<=1080]+bestaudio/best[height<=1080]', webpage_urls.pop(0)]
+    ytdl_process = run_success(ytdl_command, stdout=PIPE, text=True)
+    ytdl_results = loads(ytdl_process.stdout)
+    ytdl_formats = [f for f in ytdl_results['formats'] if f['format_id'] in ytdl_results['format_id']]
 
-for e in entries:
-    fp = NamedTemporaryFile(mode='w', delete=False)
-    dump(e, fp)
-    fp.close()
-    run([bin_ytdl, '--output', '{0}/Downloads/%(title)s-%(id)s.%(ext)s'.format(environ['HOME']), '--load-info-json', fp.name, '--format', e['format_id']])
-    remove(fp.name)
+    vlc_command = [bin_vlc, '--meta-title', ytdl_results['title'], '--meta-artist', ytdl_results['uploader'],
+        '--meta-description', ytdl_results['description'], ytdl_formats[0]['url']]
+    if len(ytdl_formats) > 1:
+        vlc_command.extend(['--input-slave', ytdl_formats[1]['url']])
+
+    Popen(vlc_command, stdout=PIPE, stderr=PIPE)
+
+for u in webpage_urls:
+    try:
+        run_success([bin_ytdl, '--output', '{0}/Downloads/%(title)s-%(id)s.%(ext)s'.format(environ['HOME']),
+            '--format', 'bestvideo[height<={0}]+bestaudio/best[height<={0}]'.format(arguments.resolution), u])
+    except KeyboardInterrupt:
+        exit()
