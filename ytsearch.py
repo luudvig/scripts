f293840d1803b4e8764b54352712f3ebf2cd0869
luudvig <ludvigolsson@outlook.com>
2020-11-25 16:52:39 +0100

    modified fedora.yml, ytsearch.py

--- ytsearch.py
+++ ytsearch.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
-from json import loads
-from os import environ
+from json import dump, loads
+from os import environ, remove
+from os.path import splitext
 from re import match
 from requests import get
 from subprocess import CalledProcessError, PIPE, Popen, run
 from sys import exit
+from tempfile import NamedTemporaryFile
 
 parser = ArgumentParser()
 parser.add_argument('-d', '--download', action='store_true', help='download all selected videos')
 parser.add_argument('-n', '--number', default=5, type=int, help='max results when searching (default: 5)')
-parser.add_argument('-r', '--resolution', default=720, type=int, help='max resolution height when downloading (default: 720)')
+parser.add_argument('-r', '--resolution', default=720, type=int, help='max resolution height (default: 720)')
 parser.add_argument('-s', '--sort', action='store_const', const='date', default='relevance', help='sort by date when searching')
 required_arguments = parser.add_argument_group('required arguments')
 required_arguments.add_argument('-k', '--api-key', required=True, help='api key to use when searching')
@@ -21,17 +23,8 @@ arguments = parser.parse_args()
 bin_ytdl = run(['which', 'youtube-dl'], stdout=PIPE, check=True, text=True).stdout.rstrip()
 bin_vlc = run(['which', 'vlc'], stdout=PIPE, check=True, text=True).stdout.rstrip()
 
-def run_success(args, stdout=None, text=None):
-    while True:
-        try:
-            process = run(args, stdout=stdout, check=True, text=text)
-        except CalledProcessError:
-            continue
-        else:
-            return process
-
 if len(arguments.query) == 1 and match('https:\/\/(www\.)?youtu\.?be(\.com)?\/(watch\?v=)?[a-zA-Z0-9-_]{11}', arguments.query[0]):
-    webpage_urls = [arguments.query[0]]
+    webpage_urls = arguments.query
 else:
     locator = 'https://youtube.googleapis.com/youtube/v3'
     headers = dict(accept='application/json')
@@ -55,22 +48,40 @@ else:
     webpage_urls = ['https://www.youtube.com/watch?v={0}'.format(videos_items[int(s) - 1]['id']) for s in selections.split()]
     print('[ytsearch] Video(s) selected: {0}'.format(' '.join(webpage_urls)))
 
-if not arguments.download:
-    ytdl_command = [bin_ytdl, '--dump-json', '--format', 'bestvideo[height<=1080]+bestaudio/best[height<=1080]', webpage_urls.pop(0)]
-    ytdl_process = run_success(ytdl_command, stdout=PIPE, text=True)
-    ytdl_results = loads(ytdl_process.stdout)
-    ytdl_formats = [f for f in ytdl_results['formats'] if f['format_id'] in ytdl_results['format_id']]
+for c, u in enumerate(webpage_urls):
+    while True:
+        try:
+            ytdl_process = run([bin_ytdl, '--dump-json', '--format', 'bestvideo[height<={0}]+bestaudio/best[height<={0}]'.format(arguments.resolution), u],
+                stdout=PIPE, check=True, text=True)
+        except CalledProcessError:
+            continue
+        except KeyboardInterrupt:
+            exit('')
+        else:
+            break
 
-    vlc_command = [bin_vlc, '--meta-title', ytdl_results['title'], '--meta-artist', ytdl_results['uploader'],
-        '--meta-description', ytdl_results['description'], ytdl_formats[0]['url']]
-    if len(ytdl_formats) > 1:
-        vlc_command.extend(['--input-slave', ytdl_formats[1]['url']])
+    ytdl_result = loads(ytdl_process.stdout)
 
-    Popen(vlc_command, stdout=PIPE, stderr=PIPE)
+    if not (c or arguments.download):
+        ytdl_formats = [f for f in ytdl_result['formats'] if f['format_id'] in ytdl_result['format_id']]
 
-for u in webpage_urls:
+        vlc_command = [bin_vlc, '--meta-title', splitext(ytdl_result['_filename'])[0], ytdl_formats[0]['url']]
+        if len(ytdl_formats) > 1:
+            vlc_command.extend(['--input-slave', ytdl_formats[1]['url']])
+
+        Popen(vlc_command, stdout=PIPE, stderr=PIPE)
+        continue
+    elif ytdl_result['is_live']:
+        print('[ytsearch] Ignoring live stream: {0}'.format(u))
+        continue
+
+    with NamedTemporaryFile(mode='w', delete=False) as ytdl_temp:
+        dump(ytdl_result, ytdl_temp)
+
     try:
-        run_success([bin_ytdl, '--output', '{0}/Downloads/%(title)s-%(id)s.%(ext)s'.format(environ['HOME']),
-            '--format', 'bestvideo[height<={0}]+bestaudio/best[height<={0}]'.format(arguments.resolution), u])
+        run([bin_ytdl, '--output', '{0}/Downloads/{1}'.format(environ['HOME'], ytdl_result['_filename']),
+            '--load-info-json', ytdl_temp.name, '--format', ytdl_result['format_id']])
     except KeyboardInterrupt:
-        exit()
+        continue
+    finally:
+        remove(ytdl_temp.name)
