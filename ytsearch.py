fc7723bc20c230089bc006defa3432edf4422077
luudvig <ludvigolsson@outlook.com>
2021-02-02 21:31:45 +0100

    modified fedora.yml, ytsearch.py

--- ytsearch.py
+++ ytsearch.py
@@ -19,23 +19,23 @@ parser.add_argument('-i', '--id', action='store_true', help='query argument is t
 parser.add_argument('-q', '--quality', default=720, type=int, choices=quality_choices, help='max quality (default: 720)', metavar='QUALITY')
 parser.add_argument('-r', '--results', default=5, type=int, choices=range(1, 51), help='max results when searching (default: 5)', metavar='RESULTS')
 parser.add_argument('-s', '--sort', action='store_const', const='date', default='relevance', help='sort by date when searching')
-required_arguments = parser.add_argument_group('required arguments')
-required_arguments.add_argument('-k', '--api-key', required=True, help='api key to use when searching')
-required_arguments.add_argument('query', nargs='+', help='query to use when searching or url to stream/download')
-arguments = parser.parse_args()
+required_args = parser.add_argument_group('required arguments')
+required_args.add_argument('-k', '--api-key', required=True, help='api key to use when searching')
+required_args.add_argument('query', nargs='+', help='query to use when searching or url to stream/download')
+args = parser.parse_args()
 
 bin_vlc, bin_ytdl = run(['which', 'vlc', 'youtube-dl'], stdout=PIPE, check=True, text=True).stdout.splitlines()
 
-if arguments.id or fullmatch('https:\/\/(www\.)?youtu\.?be(\.com)?\/(watch\?v=)?[a-zA-Z0-9-_]{11}', arguments.query[0]):
-    webpage_id = arguments.query[0][-11:]
+if args.id or fullmatch('https:\/\/(www\.)?youtu\.?be(\.com)?\/(watch\?v=)?[a-zA-Z0-9-_]{11}', args.query[0]):
+    webpage_id = args.query[0][-11:]
 else:
     locator = 'https://youtube.googleapis.com/youtube/v3'
-    headers = dict(accept='application/json')
+    headers = {'accept': 'application/json'}
 
-    search_payload = dict(part='id', maxResults=arguments.results, order=arguments.sort, q=' '.join(arguments.query), type='video', key=arguments.api_key)
+    search_payload = {'part': 'id', 'maxResults': args.results, 'order': args.sort, 'q': ' '.join(args.query), 'type': 'video', 'key': args.api_key}
     search_response = get('{0}/search'.format(locator), headers=headers, params=search_payload)
 
-    videos_payload = dict(part=['contentDetails', 'snippet'], id=[i['id']['videoId'] for i in search_response.json()['items']], key=arguments.api_key)
+    videos_payload = {'part': ['contentDetails', 'snippet'], 'id': [i['id']['videoId'] for i in search_response.json()['items']], 'key': args.api_key}
     videos_response = get('{0}/videos'.format(locator), headers=headers, params=videos_payload)
     videos_items = videos_response.json()['items']
 
@@ -50,7 +50,7 @@ else:
 
 ytdl_selector = ''.join(['bestvideo{1}[vcodec^=av01]+bestaudio{0}/best{1}{2}/bestvideo{1}{2}+bestaudio{0}/'
     .format('[ext=m4a]', '[height<={0}][height>{1}]'.format(quality_choices[c - 1], q), '[vcodec^=avc1]')
-    for c, q in enumerate(quality_choices + [0]) if q < arguments.quality])[:-1]
+    for c, q in enumerate(quality_choices + [0]) if q < args.quality])[:-1]
 
 try:
     ytdl_process = run([bin_ytdl, '--dump-json', '--format', ytdl_selector, 'https://www.youtube.com/watch?v={0}'.format(webpage_id)],
@@ -60,7 +60,7 @@ except CalledProcessError:
 
 ytdl_result = loads(ytdl_process.stdout)
 
-if not arguments.download:
+if not args.download:
     ytdl_format_ids = ytdl_result['format_id'].split('+')
     ytdl_urls = [f['url'] for f in ytdl_result['formats'] if f['format_id'] in ytdl_format_ids]
 
